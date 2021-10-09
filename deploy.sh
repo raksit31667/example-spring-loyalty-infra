0b9bc51c66e2659867987d866ea851d3aefd7d97
raksit31667 <raksit.m@ku.th>
2021-10-09 21:53:01 +0700

    :wrench: configure access to the Kubernetes API server endpoint from outside of VPC

--- deploy.sh
+++ deploy.sh
@@ -23,4 +23,5 @@ aws cloudformation deploy \
 AvailabilityZones="ap-southeast-1a,ap-southeast-1b,ap-southeast-1c" \
 RemoteAccessCIDR="124.120.16.95/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
 AdditionalEKSAdminUserArn="arn:aws:iam::564702493239:user/example-spring-loyalty" \
+EKSPublicAccessEndpoint="Enabled" \
 --no-fail-on-empty-changeset
