c9ec8ab3c92068fb39a7d2cd323c5c74c8a1a5f5
raksit31667 <raksit.m@ku.th>
2021-11-06 12:54:31 +0700

    :rocket: redeploy Amazon EKS

--- deploy.sh
+++ deploy.sh
@@ -21,13 +21,13 @@ aws cloudformation deploy \
 --template-file "s3-java-gradle-cache/template.yml" \
 --no-fail-on-empty-changeset
 
-#aws cloudformation deploy \
-#--stack-name "eks-raksit31667" \
-#--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
-#--template-file "eks-raksit31667/template.yml" \
-#--parameter-overrides \
-#AvailabilityZones="ap-southeast-1a,ap-southeast-1b,ap-southeast-1c" \
-#RemoteAccessCIDR="124.120.16.95/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
-#AdditionalEKSAdminUserArn="arn:aws:iam::564702493239:user/example-spring-loyalty" \
-#EKSPublicAccessEndpoint="Enabled" \
-#--no-fail-on-empty-changeset
+aws cloudformation deploy \
+--stack-name "eks-raksit31667" \
+--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+--template-file "eks-raksit31667/template.yml" \
+--parameter-overrides \
+AvailabilityZones="ap-southeast-1a,ap-southeast-1b,ap-southeast-1c" \
+RemoteAccessCIDR="124.120.16.95/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
+AdditionalEKSAdminUserArn="arn:aws:iam::564702493239:user/example-spring-loyalty" \
+EKSPublicAccessEndpoint="Enabled" \
+--no-fail-on-empty-changeset
