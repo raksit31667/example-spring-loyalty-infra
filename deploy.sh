ecf2ab575c2e079913cae6a440d8e28371c0e217
raksit31667 <raksit.m@ku.th>
2021-11-07 01:13:02 +0700

    :lock: add VPC security group for RDS to allow my local machine to access RDS

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ aws cloudformation deploy \
 --template-file "eks-raksit31667/template.yml" \
 --parameter-overrides \
 AvailabilityZones="ap-southeast-1a,ap-southeast-1b,ap-southeast-1c" \
-RemoteAccessCIDR="124.120.16.95/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
+RemoteAccessCIDR="119.76.14.135/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
 AdditionalEKSAdminUserArn="arn:aws:iam::564702493239:user/example-spring-loyalty" \
 EKSPublicAccessEndpoint="Enabled" \
 --no-fail-on-empty-changeset
