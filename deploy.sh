3c3dd0960cb76e289eff790dd58c5deaf70f148f
raksit31667 <raksit.m@ku.th>
2021-11-28 16:20:21 +0700

    :passport_control: connect to a private Amazon RDS DB instance from a local machine using an Amazon EC2 instance as a bastion host

--- deploy.sh
+++ deploy.sh
@@ -27,25 +27,27 @@ aws cloudformation deploy \
 --template-file "rds-postgres-example-spring-loyalty/template.yml" \
 --no-fail-on-empty-changeset
 
-aws cloudformation deploy \
---stack-name "eks-raksit31667" \
---capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
---template-file "eks-raksit31667/template.yml" \
---parameter-overrides \
-AvailabilityZones="ap-southeast-1a,ap-southeast-1b,ap-southeast-1c" \
-RemoteAccessCIDR="119.76.14.135/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
-AdditionalEKSAdminUserArn="arn:aws:iam::564702493239:user/example-spring-loyalty" \
-EKSPublicAccessEndpoint="Enabled" \
---no-fail-on-empty-changeset
+# Uncomment to deploy EKS stacks
 
-aws cloudformation deploy \
---stack-name "vpc-peering-eks-rds" \
---capabilities CAPABILITY_NAMED_IAM \
---template-file "vpc-peering-eks-rds/template.yml" \
---no-fail-on-empty-changeset
-
-aws cloudformation deploy \
---stack-name "iam-role-eks-service-account" \
---capabilities CAPABILITY_NAMED_IAM \
---template-file "iam-role-eks-service-account/template.yml" \
---no-fail-on-empty-changeset
+#aws cloudformation deploy \
+#--stack-name "eks-raksit31667" \
+#--capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+#--template-file "eks-raksit31667/template.yml" \
+#--parameter-overrides \
+#AvailabilityZones="ap-southeast-1a,ap-southeast-1b,ap-southeast-1c" \
+#RemoteAccessCIDR="119.76.14.135/32" KeyPairName="eks-raksit31667" EKSClusterName="raksit31667" \
+#AdditionalEKSAdminUserArn="arn:aws:iam::564702493239:user/example-spring-loyalty" \
+#EKSPublicAccessEndpoint="Enabled" \
+#--no-fail-on-empty-changeset
+#
+#aws cloudformation deploy \
+#--stack-name "vpc-peering-eks-rds" \
+#--capabilities CAPABILITY_NAMED_IAM \
+#--template-file "vpc-peering-eks-rds/template.yml" \
+#--no-fail-on-empty-changeset
+#
+#aws cloudformation deploy \
+#--stack-name "iam-role-eks-service-account" \
+#--capabilities CAPABILITY_NAMED_IAM \
+#--template-file "iam-role-eks-service-account/template.yml" \
+#--no-fail-on-empty-changeset
