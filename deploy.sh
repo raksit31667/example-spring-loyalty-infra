6df221459bfb0f66dd441927a6b9c92588e7b213
raksit31667 <raksit.m@ku.th>
2022-02-27 21:27:04 +0700

    :card_file_box: create the pgaudit extension to audit RDS DB instance

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,8 @@ do
   if [[ $directory_name == 'eks-raksit31667' ]]; then
     continue
   fi
+
+  echo "Deploying $directory_name stack..."
   
   aws cloudformation deploy \
   --stack-name $directory_name \
@@ -19,6 +21,42 @@ do
   --template-file "$directory_name/template.yml" \
   --no-fail-on-empty-changeset
 done
+
+rds_instance_identifier="example-spring-loyalty-db"
+
+rds_password="$(aws secretsmanager get-secret-value --secret-id ExampleSpringLoyaltyPostgreSQLSecret --query
+SecretString --output text | jq -r '.DB_PASSWORD')"
+
+rds_host="$(aws rds describe-db-instances --db-instance-identifier $rds_instance_identifier | jq -r '.DBInstances[0]
+.Endpoint.Address')"
+
+function run_psql_command() {
+  local command=$1
+
+  # https://github.com/jbergknoff/Dockerfile/tree/master/postgresql-client
+  docker run --env PGPASSWORD="$rds_password" -it --rm jbergknoff/postgresql-client \
+    --host="$rds_host" \
+    --port=5432 \
+    --dbname=ExampleSpringLoyalty \
+    -c "$command"
+}
+
+echo "Create RDS PostgreSQL role for pgaudit"
+run_psql_command "DO \$\$
+  BEGIN
+  CREATE ROLE rds_pgaudit;
+  EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+  END
+  \$\$;"
+
+echo "Rebooting RDS instance"
+aws rds reboot-db-instance --db-instance-identifier $rds_instance_identifier > /dev/null 2>&1 &
+
+echo "Waiting for RDS to become available"
+aws rds wait db-instance-available --db-instance-identifier $rds_instance_identifier
+
+echo "Creating pgaudit extension"
+run_psql_command 'CREATE EXTENSION IF NOT EXISTS pgaudit CASCADE;'
 
 # Uncomment to deploy EKS stacks
 # Ref: https://s3.amazonaws.com/aws-quickstart/quickstart-amazon-eks/templates/amazon-eks-entrypoint-new-vpc.template.yaml
