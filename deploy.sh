f16ab6bb49dc8218cc4f9e82f86257896c5af146
raksit31667 <raksit.m@ku.th>
2022-03-01 22:50:00 +0700

    :poop: move post-deployment script to a separated script since we can't connect RDS via Buildkite agent

--- deploy.sh
+++ deploy.sh
@@ -22,42 +22,6 @@ do
   --no-fail-on-empty-changeset
 done
 
-rds_instance_identifier="example-spring-loyalty-db"
-
-rds_password="$(aws secretsmanager get-secret-value --secret-id ExampleSpringLoyaltyPostgreSQLSecret \
---query SecretString --output text | jq -r '.password')"
-
-rds_host="$(aws rds describe-db-instances --db-instance-identifier $rds_instance_identifier | jq -r '.DBInstances[0]
-.Endpoint.Address')"
-
-function run_psql_command() {
-  local command=$1
-
-  # https://github.com/jbergknoff/Dockerfile/tree/master/postgresql-client
-  docker run --env PGPASSWORD="$rds_password" -it --rm jbergknoff/postgresql-client \
-    --host="$rds_host" \
-    --port=5432 \
-    --dbname=ExampleSpringLoyalty \
-    -c "$command"
-}
-
-echo "Create RDS PostgreSQL role for pgaudit"
-run_psql_command "DO \$\$
-  BEGIN
-  CREATE ROLE rds_pgaudit;
-  EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-  END
-  \$\$;"
-
-echo "Rebooting RDS instance"
-aws rds reboot-db-instance --db-instance-identifier $rds_instance_identifier > /dev/null 2>&1 &
-
-echo "Waiting for RDS to become available"
-aws rds wait db-instance-available --db-instance-identifier $rds_instance_identifier
-
-echo "Creating pgaudit extension"
-run_psql_command 'CREATE EXTENSION IF NOT EXISTS pgaudit CASCADE;'
-
 # Uncomment to deploy EKS stacks
 # Ref: https://s3.amazonaws.com/aws-quickstart/quickstart-amazon-eks/templates/amazon-eks-entrypoint-new-vpc.template.yaml
 
