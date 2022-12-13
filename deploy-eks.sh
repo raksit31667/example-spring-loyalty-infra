82ee8e87e412adcfa29f775cf1a265b6787f42e3
raksit31667 <raksit.m@ku.th>
2022-12-13 16:56:40 +0700

    :pencil2: correct EKS-related Cloudformation related template file paths

--- deploy-eks.sh
+++ deploy-eks.sh
@@ -5,17 +5,17 @@
 aws cloudformation deploy \
 --stack-name "eks-raksit31667" \
 --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
---template-file "/eks/eks-raksit31667/template.yml" \
+--template-file "eks/eks-raksit31667/template.yml" \
 --no-fail-on-empty-changeset
 
 aws cloudformation deploy \
 --stack-name "iam-role-eks-service-account" \
 --capabilities CAPABILITY_NAMED_IAM \
---template-file "/eks/iam-role-eks-service-account/template.yml" \
+--template-file "eks/iam-role-eks-service-account/template.yml" \
 --no-fail-on-empty-changeset
 
 aws cloudformation deploy \
 --stack-name "vpc-peering-eks-rds" \
 --capabilities CAPABILITY_NAMED_IAM \
---template-file "/eks/vpc-peering-eks-rds/template.yml" \
+--template-file "eks/vpc-peering-eks-rds/template.yml" \
 --no-fail-on-empty-changeset
